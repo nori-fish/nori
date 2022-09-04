b73efd6093489258a16c1825e61bf2b90ded0a8a
Jim O'Brien <git@jimmed.net>
2022-09-04 15:50:37 +0100

    fix: broken entry in _nori_list

--- functions/_nori_list.fish
+++ functions/_nori_list.fish
@@ -1,6 +1,7 @@
 function _nori_list --argument-names plugin
     set --local pattern "^$NORI_REMOTE"
-    test "$plugin"; and set --local pattern "$pattern/$plugin"
-    echo $plugin / $pattern
+    if test "$plugin"
+        set --local pattern "$pattern/$plugin"
+    end
     fisher list $pattern | grep -v nori | sed -e "s#^$NORI_REMOTE/##g"
 end
