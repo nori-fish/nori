8fc12950217335f5c8fa863e6150a626204c1d5a
Jim O'Brien <git@jimmed.net>
2022-09-04 15:55:56 +0100

    fix: correctly remove nori plugin from nori list

--- functions/_nori_list.fish
+++ functions/_nori_list.fish
@@ -3,5 +3,5 @@ function _nori_list --argument-names plugin
     if test "$plugin"
         set --local pattern "$pattern/$plugin"
     end
-    fisher list $pattern | grep -v nori | sed -e "s#^$NORI_REMOTE/##g"
+    fisher list $pattern | grep -v "$NORI_REMOTE/nori" | sed -e "s#^$NORI_REMOTE/##g"
 end
